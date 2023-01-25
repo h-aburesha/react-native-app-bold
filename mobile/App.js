d5ea1f4334fd5aa8280d1ad87719633c725503f1
HASSAN <hassan.aburesha@gmail.com>
2023-01-25 14:58:00 +0100

    Main as top level component with Redux store on top

--- mobile/App.js
+++ mobile/App.js
@@ -1,30 +1,11 @@
 import { Provider } from "react-redux";
 import store from "./Redux/store";
-
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Main from "./Components/Main";
 
 export default function App() {
     return (
         <Provider store={store}>
-            <View style={styles.container}>
-                <Text style={styles.titleText}>Hi Mint!!</Text>
-                <StatusBar style="auto" />
-            </View>
+            <Main />
         </Provider>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        fontSize: 100,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    titleText: {
-        fontSize: 50,
-        fontWeight: "bold",
-    },
-});
